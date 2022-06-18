dca09d59bfa1341abd84487c89d6309ec52b8601
RubenRivas <rudrivas@hotmail.com>
2022-06-18 11:28:10 -0500

    Update heap removing last attribute

--- heap.h
+++ heap.h
@@ -11,9 +11,9 @@ using namespace std;
 
 using index_t = int;
 // 1. Funciones para recorrer el heap
-index_t get_left_index(index_t root_index) { return root_index * 2; }
-index_t get_right_index(index_t root_index) { return root_index * 2 + 1; }
-index_t get_root_index(index_t current_index) { return current_index / 2; }
+auto get_left_index = [](index_t root_index) { return root_index * 2; };
+auto get_right_index = [](index_t root_index) { return root_index * 2 + 1; };
+inline index_t get_root_index(index_t current_index) { return current_index / 2; }
 
 // 2. Para seleccionar el hijo o el padre para comparar
 bool has_parent(index_t current_index) { return current_index / 2 != 0; }
@@ -26,8 +26,8 @@ bool has_right_child(index_t current_index, index_t last_index) {
 
 // 3. Seleccionar el hijo
 template<typename Container>
-index_t get_selected_child(const Container& cnt, index_t current_index, index_t last_index) {
-    if (!has_right_child(current_index, last_index))
+index_t get_selected_child(const Container& cnt, index_t current_index) {
+    if (!has_right_child(current_index, std::size(cnt) - 1))
             return get_left_index(current_index);
 
     auto left_index = get_left_index(current_index);
@@ -41,6 +41,7 @@ template<typename Container>
 void percolate_up(Container& cnt, index_t current_index) {
     // Ubicar el índice de la raiz inmediata
     auto root_index = get_root_index(current_index);
+
 
     // Condiciones Base de Recursividad
         // a. Si no tiene padre se detiene
@@ -57,13 +58,13 @@ void percolate_up(Container& cnt, index_t current_index) {
 
 // 5. implementar el percolate down
 template<typename Container>
-void percolate_down(Container& cnt, index_t current_index, index_t last_index) {
+void percolate_down(Container& cnt, index_t current_index) {
     // Obtener el sub-índice del hijo mayor
-    auto selected_index = get_selected_child(cnt, current_index, last_index);
+    auto selected_index = get_selected_child(cnt, current_index);
 
     // Condiciones bases
         // a. Si no tiene hijos se detiene
-        if (!has_children(current_index, last_index)) return;
+        if (!has_children(current_index, std::size(cnt) - 1)) return;
         // b. Si el valor actual es mayor que del hijo detenerse
         if (cnt[current_index]> cnt[selected_index]) return;
 
@@ -71,15 +72,15 @@ void percolate_down(Container& cnt, index_t current_index, index_t last_index) {
     swap(cnt[current_index], cnt[selected_index]);
 
     // Condición recursiva de ubicarse en el hijo y llamar al percolate_down
-    percolate_down(cnt, selected_index, last_index);
+    percolate_down(cnt, selected_index);
 }
 
 template<typename Container>
-void heapify(Container& cnt, index_t last_index) {
+void heapify(Container& cnt) {
     // Ir al padre del ultimo
-    auto current_index = get_root_index(last_index);
+    auto current_index = get_root_index(std::size(cnt) - 1);
     while (current_index > 0) {
-        percolate_down(cnt, current_index, last_index);
+        percolate_down(cnt, current_index, std::size(cnt) - 1);
         --current_index; // ir al anterior
     }
 }
@@ -87,25 +88,25 @@ void heapify(Container& cnt, index_t last_index) {
 template <typename T>
 class heap {
     vector<T> data;
-    int last = 0;
 public:
     heap(): data(1) {}
     T top() { return data[1]; }
-    bool empty() { return last == 0; }
-    int size() { return last; }
+    bool empty() { return std::size(data) == 1; }
+    int size() { return std::size(data) - 1; }
     void push(T value) {
         // Insertar al final
         data.push_back(value);
         // Ejecutar el percolate up
-        percolate_up(data, ++last);
+        percolate_up(data, std::size(data) - 1);
     }
     void pop() {
         // Validacion de vacio
         if (empty()) return;
         // Intercambio entre el primero y el ultimo
-        swap(data[1], data[last]);
+        swap(data[1], data.back());
+        data.pop_back();
         // Ejecutar el percolate up
-        percolate_down(data, 1, --last);
+        percolate_down(data, 1);
     }
 };
 
