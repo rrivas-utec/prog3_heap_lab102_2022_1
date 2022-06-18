177cd7df5fb2cf2641ab6dc9005c2dc2021a6682
RubenRivas <rudrivas@hotmail.com>
2022-06-18 12:50:53 -0500

    sesión 18 de junio 2022

--- heap.h
+++ heap.h
@@ -25,71 +25,94 @@ bool has_right_child(index_t current_index, index_t last_index) {
 }
 
 // 3. Seleccionar el hijo
-template<typename Container>
-index_t get_selected_child(const Container& cnt, index_t current_index) {
+//template<typename Container>
+//index_t get_selected_child_(const Container& cnt, index_t current_index) {
+//    if (!has_right_child(current_index, std::size(cnt) - 1))
+//            return get_left_index(current_index);
+//
+//    auto left_index = get_left_index(current_index);
+//    auto right_index = get_right_index(current_index);
+//
+//    return cnt[left_index] > cnt[right_index]? left_index: right_index;
+//}
+
+auto get_selected_child = [](const auto& cnt, index_t current_index, auto& cmp) {
     if (!has_right_child(current_index, std::size(cnt) - 1))
-            return get_left_index(current_index);
+        return get_left_index(current_index);
 
     auto left_index = get_left_index(current_index);
     auto right_index = get_right_index(current_index);
 
-    return cnt[left_index] > cnt[right_index]? left_index: right_index;
-}
+//    std::less<> cmp;
+    return cmp(cnt[right_index], cnt[left_index]) ? left_index: right_index; // Comparación de valores
+};
 
 // 4. implementar el percolate up
-template<typename Container>
-void percolate_up(Container& cnt, index_t current_index) {
+template<typename Container, typename Compare>
+void percolate_up(Container& cnt, index_t current_index, Compare& cmp) {
     // Ubicar el índice de la raiz inmediata
     auto root_index = get_root_index(current_index);
-
-
     // Condiciones Base de Recursividad
-        // a. Si no tiene padre se detiene
-        if (!has_parent(current_index)) return;
-        // b. (Max heap) si el padre es mayor detenerse
-        if (cnt[root_index] > cnt[current_index]) return;
+    // a. Si no tiene padre se detiene
+    if (!has_parent(current_index)) return;
+    // b. (Max heap) si el padre es mayor detenerse
+//    std::less<> cmp;
+    if (cmp(cnt[current_index], cnt[root_index])) return; // Comparación de valores
 
     // Intercambio de valores entre el padre y el hijo
     swap(cnt[root_index], cnt[current_index]);
 
     // Condición Recursiva de ubicarse en el padre y llamar al percolate_up
-    percolate_up(cnt, root_index);
+    percolate_up(cnt, root_index, cmp);
 }
 
 // 5. implementar el percolate down
-template<typename Container>
-void percolate_down(Container& cnt, index_t current_index) {
+template<typename Container, typename Compare>
+void percolate_down(Container& cnt, index_t current_index, Compare& cmp) {
     // Obtener el sub-índice del hijo mayor
-    auto selected_index = get_selected_child(cnt, current_index);
+    auto selected_index = get_selected_child(cnt, current_index, cmp);
 
     // Condiciones bases
-        // a. Si no tiene hijos se detiene
-        if (!has_children(current_index, std::size(cnt) - 1)) return;
-        // b. Si el valor actual es mayor que del hijo detenerse
-        if (cnt[current_index]> cnt[selected_index]) return;
+    // a. Si no tiene hijos se detiene
+    if (!has_children(current_index, std::size(cnt) - 1)) return;
+    // b. Si el valor actual es mayor que del hijo detenerse
+//    std::less<> cmp;
+    if (cmp(cnt[selected_index], cnt[current_index])) return; // Comparación de valores
 
     // Intercambio de valores entre el padre y el hijo
     swap(cnt[current_index], cnt[selected_index]);
 
     // Condición recursiva de ubicarse en el hijo y llamar al percolate_down
-    percolate_down(cnt, selected_index);
+    percolate_down(cnt, selected_index, cmp);
 }
 
-template<typename Container>
-void heapify(Container& cnt) {
+template<typename Container, typename Compare>
+void heapify(Container& cnt, Compare cmp) {
     // Ir al padre del ultimo
     auto current_index = get_root_index(std::size(cnt) - 1);
     while (current_index > 0) {
-        percolate_down(cnt, current_index, std::size(cnt) - 1);
+        percolate_down(cnt, current_index, cmp);
         --current_index; // ir al anterior
     }
 }
 
-template <typename T>
+template <
+        typename T,
+        template <typename...> typename Container = std::vector,
+        typename Compare = less<T>
+                >
 class heap {
-    vector<T> data;
+    Container<T> data;
+    Compare cmp;
 public:
     heap(): data(1) {}
+    explicit heap(Compare cmp): data(1), cmp(cmp) {}
+    heap(std::initializer_list<T> lst): data(1 + std::size(lst)) {
+        // Copiar la lista de inicialización al data
+        copy(begin(lst), end(lst), next(begin(data)));
+        // Generar el heap utiliza el heapify
+        heapify(data, cmp);
+    }
     T top() { return data[1]; }
     bool empty() { return std::size(data) == 1; }
     int size() { return std::size(data) - 1; }
@@ -97,7 +120,7 @@ public:
         // Insertar al final
         data.push_back(value);
         // Ejecutar el percolate up
-        percolate_up(data, std::size(data) - 1);
+        percolate_up(data, std::size(data) - 1, cmp);
     }
     void pop() {
         // Validacion de vacio
@@ -106,7 +129,24 @@ public:
         swap(data[1], data.back());
         data.pop_back();
         // Ejecutar el percolate up
-        percolate_down(data, 1);
+        percolate_down(data, 1, cmp);
+    }
+
+    bool replace(T old_value, T new_value) {
+        if (empty()) return false;
+        auto iter = std::find(std::next(begin(data)), std::end(data), old_value);
+        if (iter != end(data)) {
+            *iter = new_value; // Formalmente deja de ser heap
+            heapify(data, cmp);
+            return true;
+        }
+        return false;
+    }
+
+    template <typename ...Params>
+    void emplace(Params ...params) {
+        data.emplace_back(params...);
+        percolate_up(data, std::size(data) - 1, cmp);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
 #include "heap.h"
 
-int main() {
-    heap<int> h1;
-//    cout << h1.size() << endl;
-//    cout << boolalpha << h1.empty() << endl;
+struct point_t {
+    int x = 0;
+    int y = 0;
+    point_t() = default;
+    point_t(int x, int y): x(x), y(y) {}
+    friend ostream& operator<<(ostream& os, const point_t& p) {
+        os << "{" << p.x  << ", " << p.y << "}";
+        return os;
+    }
+};
 
+int main() {
+    heap<int, vector, greater<>> h1;
     h1.push(1);
     h1.push(12);
     h1.push(31);
     h1.push(4);
-
-//    cout << h1.size() << endl;
-
     while(!h1.empty()) {
         cout << h1.top() << endl;
         h1.pop();
+    }
+    cout << "----\n";
+    heap<int, vector, greater<>> h2 = {1, 20, 11, 12, 4, 25, 34, 2, 3};
+    while(!h2.empty()) {
+        cout << h2.top() << endl;
+        h2.pop();
+    }
+    cout << "----\n";
+    heap<int, vector, greater<>> h3 = {1, 20, 11, 12, 4, 25, 34, 2, 3};
+    h3.replace(34, 5);
+    while(!h3.empty()) {
+        cout << h3.top() << endl;
+        h3.pop();
+    }
+
+    auto compare_by_distance = [](auto a, auto b) {
+        return (a.x * a.x) + (a.y * a.y) > (b.x * b.x) + (b.y * b.y);
+    };
+
+    heap<point_t, vector, decltype(compare_by_distance)> h4(compare_by_distance);
+    h4.emplace(1, 1);
+    h4.emplace(2, 2);
+    h4.emplace(3, 3);
+    while(!h4.empty()) {
+        cout << h4.top() << endl;
+        h4.pop();
     }
 
     return 0;
